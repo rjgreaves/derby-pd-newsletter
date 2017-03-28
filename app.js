70a5db9a6570e175c0c84e763b5de344f8b5f3c5
Reuben Greaves <reuben.greaves@wipro.com>
2017-03-28 14:56:04 +0100

    Fixed image issue

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
