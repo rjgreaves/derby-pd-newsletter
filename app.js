ac539e164f617f8fe52567bc96a5af614d3675b9
Reuben Greaves <reuben.greaves@wipro.com>
2017-03-29 07:43:38 +0100

    Initial completion

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
-});
-
+})
 var port = process.env.PORT || 8080;
 
 var server = app.listen(port, (req, res) => {
     console.log(`Catch the action at http:\\localhost:${port}`);
-});
+})
