928da774aaa2d04fad2b56bafeb5964ab7b816d2
Reuben Greaves <reuben@yellowmouse.co.uk>
2017-07-25 08:07:45 +0100

    Started aug newsletter

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.get('/201706', (req, res) => {
 app.get('/201707', (req, res) => {
     res.sendFile(`${__dirname}/201707/index.html`);
 });
-
+app.get('/201707', (req, res) => {
+    res.sendFile(`${__dirname}/201708/index.html`);
+});
 var port = process.env.PORT || 8080;
 
 var server = app.listen(port, (req, res) => {
